acf46c4560634dfb5882efeb5b816b46aadf2cde
Do Trung Kien <trungkien.keio@gmail.com>
2018-10-02 15:17:36 +0700

    adjust betting component

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { DrizzleProvider } from 'drizzle-react'
-
 import './index.css';
+
+import options from './drizzleOptions'
 
 import App from './App';
 import LoadingContainer from './containers/LoadingContainer'
-
-import store from './store'
-
-import options from './drizzleOptions'
-// const drizzleStore = generateStore(drizzleOptions)
-// const drizzle = new Drizzle(drizzleOptions, drizzleStore)
 
 ReactDOM.render(
   <DrizzleProvider options={options}>
